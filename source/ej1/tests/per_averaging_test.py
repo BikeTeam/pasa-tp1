88a303152bf917403844c4f1582b27e30e3e47f7
scheinpablo <scheinpablo@yahoo.com>
2021-08-30 18:34:45 -0300

    Add test 2

--- source/ej1/tests/per_averaging_test.py
+++ source/ej1/tests/per_averaging_test.py
@@ -12,7 +12,16 @@ K = 40
 D = int(N/K)
 
 manual_averaging = periodogram_averaging(data,K,L)
-print(manual_averaging)
-
 f, Pxx = welch(data, nperseg = L, noverlap=L-D)
+print('Test 1')
+print(manual_averaging)
 print(Pxx)
+
+L2 = 70
+K2 = 20
+D2 = int(N/K2)
+manual_averaging2 = periodogram_averaging(data,K2,L2)
+f2, Pxx2 = welch(data, nperseg = L2, noverlap=L2-D2)
+print('Test 2')
+print(manual_averaging2)
+print(Pxx2)
