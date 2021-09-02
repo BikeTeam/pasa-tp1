f44b547f6b40601d502637bd326cc9dd9dc371e0
scheinpablo <scheinpablo@yahoo.com>
2021-09-02 18:25:24 -0300

    Update stationarity tests

--- source/helpers/unit_root.py
+++ source/helpers/unit_root.py
@@ -3,11 +3,11 @@ def check_stationarity(p_value, threshold = 0.05, type='unit'):
     print(f'p-threshold: {threshold}')
     if p_value > threshold:
         if type == 'unit':
-            print('=> Serie Estacionaria')
-        elif type == 'kpss':
-            print('=> Tendencia No Estacionaria')
-    else:
-        if type == 'unit':
             print('=> Serie No Estacionaria')
         elif type == 'kpss':
-            print('=> Tendencia Estacionaria')
+            print('=> Serie Estacionaria')
+    else:
+        if type == 'unit':
+            print('=> Serie Estacionaria')
+        elif type == 'kpss':
+            print('=> Serie No Estacionaria')
